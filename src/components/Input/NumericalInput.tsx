a47a4a50fd8a81f5d98b27482c6403e944201e49
Андрей <justjectis@gmail.com>
2021-06-03 19:53:39 +0300

    Fixed max width

--- src/components/Input/NumericalInput.tsx
+++ src/components/Input/NumericalInput.tsx
@@ -84,6 +84,7 @@ const StyledMaxButton = styled(Button)<{ isFocused?: boolean }>`
   right: ${({ isFocused }) => (isFocused ? '88' : '55')}px;
   margin-top: auto;
   margin-bottom: auto;
+  width: 45px;
 `
 
 const NumericalInput: React.FC<Props> = ({
@@ -193,7 +194,8 @@ const NumericalInput: React.FC<Props> = ({
       />
       {displayMaxButton && (
         <StyledMaxButton buttonType="max" onClick={onMaxButtonHandler} isFocused={isFocused}>
-          {t('maxAmountLabel')}
+          {/* {t('maxAmountLabel')} */}
+          最大
         </StyledMaxButton>
       )}
       {isFocused && (
