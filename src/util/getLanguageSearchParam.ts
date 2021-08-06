c32e01856a30f11c513006b3c29f69c1b9320407
jectis <justjectis@gmail.com>
2021-08-06 14:19:08 +0300

    Updated method with getDefaultLanguage

--- src/util/getLanguageSearchParam.ts
+++ src/util/getLanguageSearchParam.ts
@@ -1,12 +1,13 @@
 import { localStorageLanguageItem, urlSearchLanguageParam } from '../constants'
 import { getDefaultLanguage } from './getDefaultLanguage'
+import { availableLanguages } from '../widgets/Menu/config'
 
 // eslint-disable-next-line import/prefer-default-export
 export const getLanguageSearchParam = () => {
   const {search} = window.location
   if(search.includes(urlSearchLanguageParam)) {
     const language = search.slice(search.indexOf(urlSearchLanguageParam)+urlSearchLanguageParam.length+1)
-    if(language.toLowerCase() === 'jp' || language.toLowerCase() === 'en') {
+    if(availableLanguages.find((availableLanguage) => availableLanguage.name.toLowerCase() === language.toLowerCase())) {
       localStorage.setItem(localStorageLanguageItem, language.toUpperCase())
       return language.toLowerCase()
     }
