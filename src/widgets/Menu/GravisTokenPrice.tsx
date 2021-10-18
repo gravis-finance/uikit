ee675d393afa9bbc96dcc45cb796cb92525e98c4
jectis <justjectis@gmail.com>
2021-10-18 11:13:32 +0300

    Temp remove GRVX image

--- src/widgets/Menu/GravisTokenPrice.tsx
+++ src/widgets/Menu/GravisTokenPrice.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Flex } from '../../components/Flex'
-import grvxImage from '../../assets/images/GRVX.png'
+// import grvxImage from '../../assets/images/GRVX.png'
 import { TokenConfig } from '../../config/tokenPrice'
 
 const StyledFlex = styled(Flex)<{ mobile?: boolean }>`
@@ -35,7 +35,7 @@ const GravisTokenPrice: React.FC<Props> = ({ mobile }) => {
 
   return (
     <StyledFlex alignItems="center" title={TokenConfig.tokenPrice ? TokenConfig.tokenPrice : ''} mobile={mobile}>
-      <GravisLogo src={grvxImage} />
+      {/* <GravisLogo src={grvxImage} /> */}
       <Price>{TokenConfig.tokenPrice ? `${TokenConfig.tokenPrice}` : 'Loading...'}</Price>
     </StyledFlex>
   )
