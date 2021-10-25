dacf91ca02310c8873e80f5459838d5523205f1f
Vitaly <mirenrich@yandex.ru>
2021-10-25 22:46:52 +0300

    fix ColumnProps generic

--- src/components/DataTable/Column.tsx
+++ src/components/DataTable/Column.tsx
@@ -3,7 +3,7 @@ import { Column as CbColumn, ColumnProps as CbColumnProps } from 'cb-datatable'
 import { TableCell } from './Table'
 
 // concat Column component props with extended TableCell component
-export type ColumnProps<T = any> = CbColumnProps<T> & React.ComponentProps<typeof TableCell>
+export type ColumnProps<T> = CbColumnProps<T> & React.ComponentProps<typeof TableCell>
 
 export function Column<T>(props: ColumnProps<T>): JSX.Element {
   return <CbColumn {...props} />
