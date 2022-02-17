1db241e01b72d9f57bc135871f67500adb4c16c2
azotov <zotovprog@gmail.com>
2022-02-17 14:44:46 +0300

    Removed terms of use from menu

--- src/widgets/Menu/PanelFooter.tsx
+++ src/widgets/Menu/PanelFooter.tsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
-import { getLanguage, t } from 'react-multi-lang'
-import { SvgProps, DocumentIcon, PadlockIcon } from '../../components/Svg'
+import { getLanguage } from 'react-multi-lang'
+import { SvgProps } from '../../components/Svg'
 import Flex from '../../components/Flex/Flex'
 import Link from '../../components/Link/Link'
 import * as IconModule from './icons'
 import { socials } from './config'
-import { privacyAndPoliceLink, termsOfUseLink } from '../../constants'
 import { PanelProps, PushedProps } from './types'
-import { Tooltip } from '../../components/Tooltip'
-import GravisTokenPrice from './GravisTokenPrice'
-import { TokenConfig } from '../../config/tokenPrice'
 
 interface Props extends PanelProps, PushedProps {}
 
@@ -51,26 +47,6 @@ const SocialEntry = styled.div`
   }
 `
 
-const DocumentEntry = styled.div<{ isPushed: boolean }>`
-  display: flex;
-  align-items: flex-start;
-  flex-direction: column;
-  padding: 0 ${({ isPushed }) => (isPushed ? '45px' : '28px')};
-  margin-bottom: 50px;
-
-  a {
-    &:hover {
-      svg {
-        opacity: 0.5;
-        color: #009ce1;
-        * {
-          stroke: #009ce1;
-        }
-      }
-    }
-  }
-`
-
 const StyledExternalLink = styled(Link)`
   margin-bottom: 25px;
   svg * {
@@ -81,23 +57,6 @@ const StyledExternalLink = styled(Link)`
     svg * {
       stroke: none !important;
       fill: #009ce1;
-    }
-  }
-`
-
-const TokenContainer = styled.div`
-  display: none;
-  @media screen and (max-width: 700px) {
-    display: block;
-    > div {
-      width: 100%;
-      background: transparent;
-      padding: 0;
-      margin-bottom: 20px;
-      width: 100%;
-      > p {
-        max-width: unset;
-      }
     }
   }
 `
@@ -114,33 +73,6 @@ const PanelFooter: React.FC<Props> = ({ isPushed }) => {
 
   return (
     <Container isPushed={isPushed}>
-      <DocumentEntry isPushed={isPushed}>
-        <TokenContainer>{TokenConfig.showToken && <GravisTokenPrice />}</TokenContainer>
-        <Link href={termsOfUseLink} fontSize="14px" target="_blank">
-          {isPushed ? (
-            <DocumentIcon />
-          ) : (
-            <Tooltip title={t('Terms of Use')}>
-              <div>
-                <DocumentIcon />
-              </div>
-            </Tooltip>
-          )}
-          {isPushed && <span style={{ opacity: 0.5, marginLeft: 12 }}>{t('Terms of Use')}</span>}
-        </Link>
-        <Link mt="20px" href={privacyAndPoliceLink} fontSize="14px" target="_blank">
-          {isPushed ? (
-            <PadlockIcon />
-          ) : (
-            <Tooltip title={t('Privacy Policy')}>
-              <div>
-                <PadlockIcon />
-              </div>
-            </Tooltip>
-          )}
-          {isPushed && <span style={{ opacity: 0.5, marginLeft: 12 }}>{t('Privacy Policy')}</span>}
-        </Link>
-      </DocumentEntry>
       <SocialEntry>
         <Flex flexWrap="wrap" justifyContent="space-evenly" style={{ width: '100%' }}>
           {socials.map((social) => {
