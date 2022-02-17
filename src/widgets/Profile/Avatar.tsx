cb21d5b112634a235d10fcd586b23465a4fa8988
Vitaly <mirenrich@yandex.ru>
2022-02-17 23:30:32 +0300

    Profile components improve

--- src/widgets/Profile/Avatar.tsx
+++ src/widgets/Profile/Avatar.tsx
@@ -5,7 +5,7 @@ type AvatarProps = React.ComponentProps<typeof Box> & {
   src?: string
 }
 
-export const Avatar = ({ src, ...boxProps }: AvatarProps) => {
+export const Avatar = React.memo(({ src, ...boxProps }: AvatarProps) => {
   if (!src) return null
   return <Box as="img" src={src} borderRadius="50%" size={95} {...boxProps} />
-}
+})
--- src/widgets/Profile/ProfileName.tsx
+++ src/widgets/Profile/ProfileName.tsx
@@ -2,11 +2,9 @@ import React from 'react'
 import { Box } from '../../components/Box'
 import { useProfile, UseProfileParams } from './queries'
 
-export const ProfileName = ({
-  account,
-  children,
-  ...boxProps
-}: UseProfileParams & React.ComponentProps<typeof Box>) => {
-  const profile = useProfile({ account })
-  return <Box {...boxProps}>{!profile.isLoading && !!profile.data?.nickname ? profile.data?.nickname : children}</Box>
-}
+export const ProfileName = React.memo(
+  ({ account, children, ...boxProps }: UseProfileParams & React.ComponentProps<typeof Box>) => {
+    const profile = useProfile({ account })
+    return <Box {...boxProps}>{!profile.isLoading && !!profile.data?.nickname ? profile.data?.nickname : children}</Box>
+  }
+)
