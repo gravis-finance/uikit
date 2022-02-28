c0691e209d5c8bf2186416bfda0ad25b0684b419
darth_nihilus <darthnihilusgravis@gmail.com>
2022-02-28 11:55:06 +0300

    Add icons for assets page on evervoid

---
+++ src/components/Svg/Icons/MetamaskEmpty.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Svg from '../Svg'
+import { SvgProps } from '../types'
+
+const Icon: React.FC<SvgProps> = (props) => {
+    return (
+        <Svg width="16" height="16" viewBox="0 0 16 16" fill="none" {...props}>
+            <path d="M15.0997 1.08984L9.25586 5.43012L10.3365 2.86941L15.0997 1.08984Z" fill="#E2761B" stroke="#E2761B" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M1.60938 1.08594L7.40619 5.46732L6.37839 2.86551L1.60938 1.08594Z" fill="#E4761B" stroke="#E4761B" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M12.9968 11.1484L11.4404 13.5329L14.7705 14.4492L15.7278 11.2013L12.9968 11.1484Z" fill="#E4761B" stroke="#E4761B" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M0.992188 11.2013L1.94364 14.4492L5.27373 13.5329L3.71734 11.1484L0.992188 11.2013Z" fill="#E4761B" stroke="#E4761B" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M5.08519 7.11994L4.15723 8.52363L7.46382 8.67046L7.34636 5.11719L5.08519 7.11994Z" fill="#E4761B" stroke="#E4761B" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M11.6227 7.11808L9.33221 5.07422L9.25586 8.6686L12.5566 8.52177L11.6227 7.11808Z" fill="#E4761B" stroke="#E4761B" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M5.27441 13.5308L7.25955 12.5617L5.54458 11.2227L5.27441 13.5308Z" fill="#E4761B" stroke="#E4761B" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M9.44922 12.5617L11.4402 13.5308L11.1642 11.2227L9.44922 12.5617Z" fill="#E4761B" stroke="#E4761B" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M11.4402 13.5277L9.44922 12.5586L9.6078 13.8566L9.59018 14.4028L11.4402 13.5277Z" fill="#D7C1B3" stroke="#D7C1B3" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M5.27344 13.5277L7.12349 14.4028L7.11174 13.8566L7.25857 12.5586L5.27344 13.5277Z" fill="#D7C1B3" stroke="#D7C1B3" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M5.27319 13.5369L5.5551 11.1523L3.7168 11.2052L5.27319 13.5369Z" fill="#CD6116" stroke="#CD6116" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M11.1582 11.1523L11.4401 13.5369L12.9965 11.2052L11.1582 11.1523Z" fill="#CD6116" stroke="#CD6116" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M12.5566 8.52344L9.25586 8.67027L9.56126 10.3676L10.0487 9.34568L11.2234 9.88014L12.5566 8.52344Z" fill="#CD6116" stroke="#CD6116" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M5.49631 9.88014L6.67094 9.34568L7.15254 10.3676L7.46382 8.67027L4.15723 8.52344L5.49631 9.88014Z" fill="#CD6116" stroke="#CD6116" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M4.15625 8.52344L5.54232 11.2251L5.49533 9.88014L4.15625 8.52344Z" fill="#E4751F" stroke="#E4751F" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M11.2228 9.88014L11.1641 11.2251L12.556 8.52344L11.2228 9.88014Z" fill="#E4751F" stroke="#E4751F" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M7.46265 8.67188L7.15137 10.3692L7.539 12.372L7.62709 9.73492L7.46265 8.67188Z" fill="#E4751F" stroke="#E4751F" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M9.25428 8.67188L9.0957 9.72905L9.16618 12.372L9.55968 10.3692L9.25428 8.67188Z" fill="#E4751F" stroke="#E4751F" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M9.56049 10.3664L9.16699 12.3691L9.4489 12.5629L11.1639 11.2239L11.2226 9.87891L9.56049 10.3664Z" fill="#F6851B" stroke="#F6851B" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M5.49609 9.87891L5.54308 11.2239L7.25805 12.5629L7.53996 12.3691L7.15233 10.3664L5.49609 9.87891Z" fill="#F6851B" stroke="#F6851B" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M9.59022 14.4103L9.60784 13.8641L9.46101 13.7348H7.24682L7.11174 13.8641L7.12349 14.4103L5.27344 13.5352L5.91949 14.0637L7.2292 14.9741H9.47863L10.7942 14.0637L11.4403 13.5352L9.59022 14.4103Z" fill="#C0AD9E" stroke="#C0AD9E" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M9.44885 12.5649L9.16694 12.3711H7.54007L7.25816 12.5649L7.11133 13.8629L7.24641 13.7337H9.4606L9.60743 13.8629L9.44885 12.5649Z" fill="#161616" stroke="#161616" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M15.3459 5.70812L15.8451 3.31187L15.0992 1.08594L9.44922 5.27938L11.6223 7.11768L14.694 8.01628L15.3752 7.2234L15.0816 7.01197L15.5514 6.58322L15.1873 6.30131L15.6572 5.94305L15.3459 5.70812Z" fill="#763D16" stroke="#763D16" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M0.869141 3.31187L1.36836 5.70812L1.05121 5.94305L1.52106 6.30131L1.1628 6.58322L1.63265 7.01197L1.33899 7.2234L2.01441 8.01628L5.08608 7.11768L7.25915 5.27938L1.60916 1.08594L0.869141 3.31187Z" fill="#763D16" stroke="#763D16" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M14.6938 8.01969L11.6222 7.12109L12.556 8.52478L11.1641 11.2264L12.9965 11.2029H15.7275L14.6938 8.01969Z" fill="#F6851B" stroke="#F6851B" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M5.08579 7.12109L2.01412 8.01969L0.992188 11.2029H3.71734L5.54389 11.2264L4.15783 8.52478L5.08579 7.12109Z" fill="#F6851B" stroke="#F6851B" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M9.25578 8.66988L9.4496 5.28106L10.3423 2.86719H6.37793L7.25891 5.28106L7.46447 8.66988L7.53494 9.7388L7.54082 12.37H9.16769L9.17943 9.7388L9.25578 8.66988Z" fill="#F6851B" stroke="#F6851B" strokeWidth="0.0587317" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M9.30078 10.6447L10.2418 8.67188L12.5094 9.70363L9.30078 10.6447Z" fill="#233447" stroke="#233447" strokeWidth="0.113379" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M7.46289 10.6447L6.52184 8.67188L4.25425 9.70363L7.46289 10.6447Z" fill="#233447" stroke="#233447" strokeWidth="0.113379" strokeLinecap="round" strokeLinejoin="round"/>
+        </Svg>
+    )
+}
+
+export default Icon
