eda3a3d4e8a8ca5a5f81c01dbf3817e4452efd33
darth_nihilus <darthnihilusgravis@gmail.com>
2022-03-18 10:56:12 +0300

    Update privacy policy and terms of use links

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,18 +5,18 @@ export const cookiesAcceptedParam = 'cookiesAccepted'
 
 export const comparedNetworksIds = [
   {
-    name: "huobi",
+    name: 'huobi',
     networks: [128, 256],
   },
   {
-    name: "binance",
+    name: 'binance',
     networks: [56, 97],
   },
   {
-    name: "polygon",
+    name: 'polygon',
     networks: [137, 80001],
-  }
+  },
 ]
 
-export const termsOfUseLink = 'https://gateway.pinata.cloud/ipfs/QmZkaXtp5Qzwnxh4NCHxwYy6ZzYM3RSAfHKzcsv9U8z5CU'
-export const privacyAndPoliceLink = 'https://gateway.pinata.cloud/ipfs/QmPLLfThJAabGwGxEx9xSwJLcjp8bfRe5MccT38enm9YQq'
+export const termsOfUseLink = 'https://drive.google.com/file/d/1p5n2HUo3VNUhhbr60cKkjP_8dBHbMO4h/view?usp=sharing'
+export const privacyAndPoliceLink = 'https://drive.google.com/file/d/1vWi8Zx-TD190to9LT8HP9Dhy5_IIj840/view?usp=sharing'
