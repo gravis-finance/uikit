171bb04f4b07b8b30a7372be106ae107fd3b8ef6
darth_nihilus <darthnihilusgravis@gmail.com>
2022-05-23 15:50:24 +0300

    Add breakpoint

--- src/theme/base.ts
+++ src/theme/base.ts
@@ -6,9 +6,12 @@ export const breakpointMap: { [key: string]: number } = {
   md: 852,
   lg: 968,
   xl: 1080,
+  xxl: 1144
 }
 
-const breakpoints: Breakpoints = Object.values(breakpointMap).map((breakpoint) => `${breakpoint}px`)
+const breakpoints: Breakpoints = Object.values(breakpointMap).map(
+  (breakpoint) => `${breakpoint}px`
+)
 
 const mediaQueries: MediaQueries = {
   xs: `@media screen and (min-width: ${breakpointMap.xs}px)`,
@@ -16,16 +19,18 @@ const mediaQueries: MediaQueries = {
   md: `@media screen and (min-width: ${breakpointMap.md}px)`,
   lg: `@media screen and (min-width: ${breakpointMap.lg}px)`,
   xl: `@media screen and (min-width: ${breakpointMap.xl}px)`,
-  nav: `@media screen and (min-width: ${breakpointMap.lg}px)`,
+  xxl: `@media screen and (min-width: ${breakpointMap.xxl}px)`,
+  nav: `@media screen and (min-width: ${breakpointMap.lg}px)`
 }
 
 export const shadows = {
-  level1: '0px 2px 12px -8px rgba(25, 19, 38, 0.1), 0px 1px 1px rgba(25, 19, 38, 0.05)',
+  level1:
+    '0px 2px 12px -8px rgba(25, 19, 38, 0.1), 0px 1px 1px rgba(25, 19, 38, 0.05)',
   active: '0px 0px 0px 1px #0098A1, 0px 0px 4px 8px rgba(31, 199, 212, 0.4)',
   success: '0px 0px 0px 1px #31D0AA, 0px 0px 0px 4px rgba(49, 208, 170, 0.2)',
   warning: '0px 0px 0px 1px #ED4B9E, 0px 0px 0px 4px rgba(237, 75, 158, 0.2)',
   focus: '0px 0px 0px 1px #7645D9, 0px 0px 0px 4px rgba(118, 69, 217, 0.6)',
-  inset: 'inset 0px 2px 2px -1px rgba(74, 74, 104, 0.1)',
+  inset: 'inset 0px 2px 2px -1px rgba(74, 74, 104, 0.1)'
 }
 
 const spacing: Spacing = [0, 4, 8, 16, 24, 32, 48, 64]
@@ -34,12 +39,12 @@ const radii = {
   small: '4px',
   default: '4px',
   card: '32px',
-  circle: '50%',
+  circle: '50%'
 }
 
 const zIndices = {
   dropdown: 10,
-  modal: 100,
+  modal: 100
 }
 
 export default {
@@ -49,5 +54,5 @@ export default {
   spacing,
   shadows,
   radii,
-  zIndices,
+  zIndices
 }
