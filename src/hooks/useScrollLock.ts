f05618b391a598435400dc0bf2b8fa3809cdb249
Vitaly <mirenrich@yandex.ru>
2022-07-14 11:44:42 +0300

    scroll lock improve

--- src/hooks/useScrollLock.ts
+++ src/hooks/useScrollLock.ts
@@ -16,8 +16,14 @@ export const useScrollLock = ({
     ) {
       const scrollbarWidth =
         window.innerWidth - document.documentElement.clientWidth
-
+      document.body.style.top = `-${window.scrollY}px`
+      document.body.style.left = '0'
       document.body.style.overflow = 'hidden'
+      document.body.style.position = 'fixed'
+      document.body.style.width = '100%'
+      // trick for IOS safari
+      document.documentElement.style.height = `${window.innerHeight - 1}px`
+
       if (scrollbarWidth) {
         document.body.style.paddingRight = `${scrollbarWidth}px`
       }
@@ -26,8 +32,15 @@ export const useScrollLock = ({
 
     return () => {
       if (updated) {
+        const top = Number(document.body.style.top?.replace('px', ''))
         document.body.style.overflow = ''
         document.body.style.paddingRight = ''
+        document.body.style.position = ''
+        document.body.style.left = ''
+        document.body.style.top = ''
+        document.body.style.width = ''
+        document.documentElement.style.height = ''
+        window.scrollTo(0, Math.abs(top))
       }
     }
   }, [open, disableScrollLock])
