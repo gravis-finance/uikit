1f3c471d7f4bbd6ab35e933ad1077a896f7d8e33
igag <bigbag@frfi.io>
2022-12-13 15:24:55 +0300

    Fix

--- src/util/useGetMenuLinks.ts
+++ src/util/useGetMenuLinks.ts
@@ -6,49 +6,54 @@ import { MenuEntry } from '../widgets/Menu'
 import { getNetworkId, getNetworkTitles } from './getNetworkId'
 
 const useGetMenuLinks = (menuLinks: MenuEntry[]): MenuEntry[] => {
-  const t = useTranslation()
-  const chainId = getNetworkId()
-  const onlyBscLabels = useMemo(
-    () => [
-      t('buyNFT'),
-      t('sellNFT'),
-      t('sendNFT'),
-      t('Activity'),
-      t('mainMenu.NFTFarming')
-    ],
-    [t]
-  )
+    const t = useTranslation()
+    const chainId = getNetworkId()
+    const onlyBscLabels = useMemo(
+        () => [
+            t('buyNFT'),
+            t('sellNFT'),
+            t('sendNFT'),
+            t('Activity'),
+            t('mainMenu.NFTFarming')
+        ],
+        [t]
+    )
 
-  return useMemo(
-    () =>
-      [...menuLinks].map((link) => {
-        const newLink = { ...link }
-        newLink.data = newLink.label
-        newLink.label = t(newLink.label)
-        newLink.href = `${
-          newLink.href
-        }?network=${chainId}`
-        if (newLink.items) {
-          newLink.items = newLink.items.map((item) => {
-            const newItem = { ...item }
-            newItem.label = t(newItem.label)
-            if (!onlyBscLabels.includes(newItem.label)) {
-              if (newItem.label === t('mainMenu.analytics.analytics'))
-                newItem.href = `${
-                  newItem.href
-                }?network=${getNetworkTitles()?.toLowerCase()}}`
-              else
-                newItem.href = `${
-                  newItem.href
-                }?network=${chainId}`
-            }
-            return newItem
-          })
-        }
-        return newLink
-      }),
-    [chainId, menuLinks, onlyBscLabels, t]
-  )
+    return useMemo(
+        () =>
+            [...menuLinks].map((link) => {
+                const newLink = { ...link }
+                newLink.data = newLink.label
+                newLink.label = t(newLink.label)
+                newLink.href = `${
+                    newLink.href
+                }?network=${chainId}&${urlSearchLanguageParam}=${t('language')}`
+                if (newLink.items) {
+                    newLink.items = newLink.items.map((item) => {
+                        const newItem = { ...item }
+                        newItem.label = t(newItem.label)
+                        if (!onlyBscLabels.includes(newItem.label)) {
+                            if (newItem.label === t('mainMenu.analytics.analytics'))
+                                newItem.href = `${
+                                    newItem.href
+                                }?network=${getNetworkTitles()?.toLowerCase()}&${urlSearchLanguageParam}=${t(
+                                    'language'
+                                )}`
+                            else
+                                newItem.href = `${
+                                    newItem.href
+                                }?network=${chainId}&${urlSearchLanguageParam}=${t('language')}`
+                        }
+                        return newItem
+                    })
+                }
+                return {
+                    ...newLink,
+                    href: newLink.href.replace('}', '')
+                }
+            }),
+        [chainId, menuLinks, onlyBscLabels, t]
+    )
 }
 
 export default useGetMenuLinks
